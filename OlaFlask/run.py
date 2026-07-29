a4f9849f69a9252fed0f94a4db1f589843e7cbf2
Talyson Souza <Talysonsouzagomes10@gmail.com>
2026-07-28 21:14:44 -0300

    Clear

--- OlaFlask/run.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/<numero>', methods=['POST', 'GET'])
-def ola(numero):
-    return f"olá mundo lindo do Talyon {numero}"
-
-if __name__ == '__main__':
-    app.run(debug=True)
